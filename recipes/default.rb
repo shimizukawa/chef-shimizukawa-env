24157e140b2b8294d8cce0ad1a0adff98e08c8dd
shimizukawa <shimizukawa at gmail dot com>
2013-02-10 19:06:27 +0900

    add .vim files

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,11 +3,28 @@ package "screen"
 package "vim"
 
 owner = 'vagrant'
+group = 'vagrant'
 
 cookbook_file "/home/#{owner}/.screenrc" do
   source 'screenrc.dot'
   action :create
   owner owner
+  group group
+  mode "0664"
+end
+
+remote_directory "/home/#{owner}/.vim" do
+  source '.vim'
+  owner owner
+  group group
+  mode "0755"
+end
+
+cookbook_file "/home/#{owner}/.vimrc" do
+  source '.vimrc'
+  action :create
+  owner owner
+  group group
   mode "0664"
 end
 
