f0cc67f55be7cba4a4894a56e54cd7972af14e98
shimizukawa <shimizukawa@gmail.com>
2014-12-05 19:32:50 +0900

    add name metadata field

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name             "shimizukawa-env"
 maintainer       "Takayuki SHIMIZUKAWA"
 maintainer_email "shimizukawa@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures shimizukawa-env"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.1.3"
